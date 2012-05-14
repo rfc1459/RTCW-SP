6b56e786c6bc7925ca49e0ded730d1a19f490c05
Matteo Panella <morpheus@level28.org>
2012-05-14 15:40:48 +0000

    Goodbye OSS, welcome SDL!

--- src/unix/linux_snd.c
+++ src/unix/linux_snd.c
@@ -2,9 +2,9 @@
 ===========================================================================
 
 Return to Castle Wolfenstein single player GPL Source Code
-Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company. 
+Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company.
 
-This file is part of the Return to Castle Wolfenstein single player GPL Source Code (RTCW SP Source Code).  
+This file is part of the Return to Castle Wolfenstein single player GPL Source Code ("RTCW SP Source Code").
 
 RTCW SP Source Code is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -27,35 +27,51 @@ If you have questions concerning this license or the applicable additional terms
 */
 
 #include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/shm.h>
-#include <sys/wait.h>
-#ifdef __linux__ // rb0101023 - guard this
-#include <linux/soundcard.h>
-#endif
-#ifdef __FreeBSD__ // rb0101023 - added
-#include <sys/soundcard.h>
-#endif
 #include <stdio.h>
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_audio.h>
+
+#include <dlfcn.h>
 
 #include "../game/q_shared.h"
 #include "../client/snd_local.h"
 
-int audio_fd;
 int snd_inited = 0;
+
+typedef struct
+{
+	void *SDLLib;
+
+	int dma_pos;
+	int dma_size;
+
+	qboolean sdl_lib_loaded;
+} sdlstate_t;
+
+static sdlstate_t sdl_state;
+
+// SDL.h
+int (*__SDL_Init) (Uint32 flags);
+Uint32 (*__SDL_WasInit) (Uint32 flags);
+char *(*__SDL_GetError) (void);
+void (*__SDL_QuitSubSystem) (Uint32 flags);
+
+// SDL_audio.h
+int (*__SDL_OpenAudio) (SDL_AudioSpec *desired, SDL_AudioSpec *obtained);
+char *(*__SDL_AudioDriverName) (char *namebuf, int maxlen);
+void (*__SDL_PauseAudio) (int pause_on);
+void (*__SDL_LockAudio) (void);
+void (*__SDL_UnlockAudio) (void);
+void (*__SDL_CloseAudio) (void);
+
+static void sdl_callback(void *userdata, Uint8 *stream, int len);
 
 cvar_t *sndbits;
 cvar_t *sndspeed;
 cvar_t *sndchannels;
-
-cvar_t *snddevice;
-
-/* Some devices may work only with 48000 */
-static int tryrates[] = { 22050, 11025, 44100, 48000, 8000 };
+cvar_t *sdlsamplesmult;
 
 static qboolean use_custom_memset = qfalse;
 // show_bug.cgi?id=371
@@ -76,223 +92,155 @@ void Snd_Memset( void* dest, const int val, const size_t count ) {
 }
 
 qboolean SNDDMA_Init( void ) {
-	int rc;
-	int fmt;
-	int tmp;
-	int i;
-	// char *s; // bk001204 - unused
-	struct audio_buf_info info;
-	int caps;
-	extern uid_t saved_euid;
+	const char *dllname;
+	SDL_AudioSpec desired, obtained;
+	char drivername[128];
+	int samples;
 
 	if ( snd_inited ) {
 		return 1;
 	}
 
-	if ( !snddevice ) {
+	if ( !sndbits ) {
 		sndbits = Cvar_Get( "sndbits", "16", CVAR_ARCHIVE );
 		sndspeed = Cvar_Get( "sndspeed", "0", CVAR_ARCHIVE );
 		sndchannels = Cvar_Get( "sndchannels", "2", CVAR_ARCHIVE );
-		snddevice = Cvar_Get( "snddevice", "/dev/dsp", CVAR_ARCHIVE );
+		sdlsamplesmult = Cvar_Get( "sdlsamplesmult", "8", CVAR_ARCHIVE );
 	}
 
-	// open /dev/dsp, confirm capability to mmap, and get size of dma buffer
-	if ( !audio_fd ) {
-		seteuid( saved_euid );
+	// FIXME: potential security problem!
+	dllname = getenv("RTCW_SDL_LIB");
+	if ( dllname == NULL ) {
+		dllname = "libSDL.so";
+	}
 
-		audio_fd = open( snddevice->string, O_RDWR );
+	if ( !sdl_state.sdl_lib_loaded ) {
+		if ( ( sdl_state.SDLLib = dlopen( dllname, RTLD_LAZY | RTLD_GLOBAL ) ) == 0 ) {
+			Com_Printf( "SNDDMA_Init: Can't load %s from /etc/ld.so.conf or current dir: %s\n", dllname, dlerror() );
+			return qfalse;
+		}
 
-		seteuid( getuid() );
+		#define DLSYM_FUNC(type, func) \
+		if ( ( __##func = (type) dlsym( sdl_state.SDLLib, #func ) ) == NULL ) { \
+			Com_Printf( "SNDDMA_Init: Could not load %s: %s\n", #func, dlerror() ); \
+			dlclose( sdl_state.SDLLib ); \
+			sdl_state.SDLLib = NULL; \
+			return qfalse; \
+		}
+		DLSYM_FUNC(int (*) (Uint32), SDL_Init)
+		DLSYM_FUNC(Uint32 (*) (Uint32), SDL_WasInit)
+		DLSYM_FUNC(char *(*) (void), SDL_GetError)
+		DLSYM_FUNC(void (*) (Uint32), SDL_QuitSubSystem)
+		DLSYM_FUNC(int (*) (SDL_AudioSpec *, SDL_AudioSpec *), SDL_OpenAudio)
+		DLSYM_FUNC(char *(*) (char *, int), SDL_AudioDriverName)
+		DLSYM_FUNC(void (*) (int), SDL_PauseAudio)
+		DLSYM_FUNC(void (*) (void), SDL_LockAudio)
+		DLSYM_FUNC(void (*) (void), SDL_UnlockAudio)
+		DLSYM_FUNC(void (*) (void), SDL_CloseAudio)
+		#undef DLSYM_FUNC
+		sdl_state.sdl_lib_loaded = qtrue;
+	}
 
-		if ( audio_fd < 0 ) {
-			perror( snddevice->string );
-			Com_Printf( "Could not open %s\n", snddevice->string );
-			return 0;
+	if ( !__SDL_WasInit(SDL_INIT_AUDIO) ) {
+		if ( __SDL_Init(SDL_INIT_AUDIO) == -1 ) {
+			Com_Printf( "SNDDMA_Init: SDL_Init(SDL_INIT_AUDIO) failed: %s\n",  __SDL_GetError() );
+			return qfalse;
 		}
 	}
 
-	if ( ioctl( audio_fd, SNDCTL_DSP_GETCAPS, &caps ) == -1 ) {
-		perror( snddevice->string );
-		Com_Printf( "Sound driver too old\n" );
-		close( audio_fd );
-		return 0;
+	if ( __SDL_AudioDriverName( drivername, sizeof(drivername) ) == NULL ) {
+		Com_Printf( "SNDDMA_Init: SDL audio driver name is NULL\n" );
+		__SDL_QuitSubSystem(SDL_INIT_AUDIO);
+		return qfalse;
 	}
 
-	if ( !( caps & DSP_CAP_TRIGGER ) || !( caps & DSP_CAP_MMAP ) ) {
-		Com_Printf( "Sorry but your soundcard can't do this\n" );
-		close( audio_fd );
-		return 0;
-	}
+	memset( &desired, '\0', sizeof(desired) );
+	memset( &obtained, '\0', sizeof(obtained) );
 
+	desired.freq = (int) sndspeed->value;
 
-	/* SNDCTL_DSP_GETOSPACE moved to be called later */
+	if ( !desired.freq )
+		desired.freq = 44100;
 
-	// set sample bits & speed
-	dma.samplebits = (int)sndbits->value;
-	if ( dma.samplebits != 16 && dma.samplebits != 8 ) {
-		ioctl( audio_fd, SNDCTL_DSP_GETFMTS, &fmt );
-		if ( fmt & AFMT_S16_LE ) {
-			dma.samplebits = 16;
-		} else if ( fmt & AFMT_U8 ) {
-			dma.samplebits = 8;
-		}
-	}
-
-	dma.speed = (int)sndspeed->value;
-	if ( !dma.speed ) {
-		for ( i = 0 ; i < sizeof( tryrates ) / 4 ; i++ )
-			if ( !ioctl( audio_fd, SNDCTL_DSP_SPEED, &tryrates[i] ) ) {
-				break;
-			}
-		dma.speed = tryrates[i];
-	}
-
-	dma.channels = (int)sndchannels->value;
-	if ( dma.channels < 1 || dma.channels > 2 ) {
-		dma.channels = 2;
-	}
-
-/*  mmap() call moved forward */
-
-	tmp = 0;
-	if ( dma.channels == 2 ) {
-		tmp = 1;
-	}
-	rc = ioctl( audio_fd, SNDCTL_DSP_STEREO, &tmp );
-	if ( rc < 0 ) {
-		perror( snddevice->string );
-		Com_Printf( "Could not set %s to stereo=%d", snddevice->string, dma.channels );
-		close( audio_fd );
-		return 0;
-	}
-
-	if ( tmp ) {
-		dma.channels = 2;
+	if ( (int) sndbits->value == 8 ) {
+		desired.format = AUDIO_U8;
 	} else {
-		dma.channels = 1;
+		desired.format = AUDIO_S16SYS;
 	}
 
-	rc = ioctl( audio_fd, SNDCTL_DSP_SPEED, &dma.speed );
-	if ( rc < 0 ) {
-		perror( snddevice->string );
-		Com_Printf( "Could not set %s speed to %d", snddevice->string, dma.speed );
-		close( audio_fd );
-		return 0;
-	}
-
-	if ( dma.samplebits == 16 ) {
-		rc = AFMT_S16_LE;
-		rc = ioctl( audio_fd, SNDCTL_DSP_SETFMT, &rc );
-		if ( rc < 0 ) {
-			perror( snddevice->string );
-			Com_Printf( "Could not support 16-bit data.  Try 8-bit.\n" );
-			close( audio_fd );
-			return 0;
-		}
-	} else if ( dma.samplebits == 8 ) {
-		rc = AFMT_U8;
-		rc = ioctl( audio_fd, SNDCTL_DSP_SETFMT, &rc );
-		if ( rc < 0 ) {
-			perror( snddevice->string );
-			Com_Printf( "Could not support 8-bit data.\n" );
-			close( audio_fd );
-			return 0;
-		}
+	if ( desired.freq <= 11025 ) {
+		desired.samples = 256;
+	} else if ( desired.freq <= 22050 ) {
+		desired.samples = 512;
+	} else if ( desired.freq <= 44100 ) {
+		desired.samples = 1024;
 	} else {
-		perror( snddevice->string );
-		Com_Printf( "%d-bit sound not supported.", dma.samplebits );
-		close( audio_fd );
-		return 0;
+		desired.samples = 2048;
 	}
 
-	if ( ioctl( audio_fd, SNDCTL_DSP_GETOSPACE, &info ) == -1 ) {
-		perror( "GETOSPACE" );
-		Com_Printf( "Um, can't do GETOSPACE?\n" );
-		close( audio_fd );
-		return 0;
+	desired.channels = (int) sndchannels->value;
+	desired.callback = (void (*) (void*, Uint8*, int)) sdl_callback;
+
+	if ( __SDL_OpenAudio( &desired, &obtained ) == -1 ) {
+		Com_Printf( "SNDDMA_Init: SDL_OpenAudio() failed: %s\n", __SDL_GetError() );
+		__SDL_QuitSubSystem(SDL_INIT_AUDIO);
+		return qfalse;
 	}
 
-	dma.samples = info.fragstotal * info.fragsize / ( dma.samplebits / 8 );
+	samples = obtained.samples * obtained.channels;
+
+	if ((int) sdlsamplesmult->value >= 1) {
+		samples *= (int) sdlsamplesmult->value;
+	}
+
+	// make it power of two
+	if (samples & (samples - 1)) {
+		int val = 1;
+		while (val < samples) {
+			val <<= 1;
+		}
+		samples = val;
+	}
+
+	dma.samplebits = obtained.format & 0xff; // first byte of format is bits
+	dma.channels = obtained.channels;
+	dma.samples = samples;
 	dma.submission_chunk = 1;
+	dma.speed = obtained.freq;
 
-	// memory map the dma buffer
-	// TTimo PROT_READ
-	// show_bug.cgi?id=371
-	// checking Alsa bug, doesn't allow dma alloc with PROT_READ?
+	sdl_state.dma_pos = 0;
+	sdl_state.dma_size = ( dma.samples * ( dma.samplebits / 8 ) );
 
-	// 1-17-2002
-	// use local C implementation of memset in all cases
-	// propagated from MP code to be safe with sound stuff once and for all
+	dma.buffer = (byte *) malloc( sdl_state.dma_size );
+	memset( dma.buffer, '\0', sdl_state.dma_size );
 
-/*
-	if (!dma.buffer)
-		dma.buffer = (unsigned char *) mmap(NULL, info.fragstotal
-			* info.fragsize, PROT_WRITE|PROT_READ, MAP_FILE|MAP_SHARED, audio_fd, 0);
-*/
-	dma.buffer = NULL;
-
-	if ( dma.buffer == NULL ) {
-//    Com_Printf("Could not mmap dma buffer PROT_WRITE|PROT_READ\n");
-//    Com_Printf("trying mmap PROT_WRITE (with associated better compatibility / less performance code)\n");
-		dma.buffer = (unsigned char *) mmap( NULL, info.fragstotal
-											 * info.fragsize, PROT_WRITE, MAP_FILE | MAP_SHARED, audio_fd, 0 );
-		// LordHavoc MAP_FAILED is a bad value to have outside init code
-		if (dma.buffer == MAP_FAILED)
-			dma.buffer = NULL;
-		// NOTE TTimo could add a variable to force using regular memset on systems that are known to be safe
-		use_custom_memset = qtrue;
-	}
-
-	if ( dma.buffer == NULL ) {
-		perror( snddevice->string );
-		Com_Printf( "Could not mmap %s\n", snddevice->string );
-		close( audio_fd );
-		return 0;
-	}
-
-	// toggle the trigger & start her up
-
-	tmp = 0;
-	rc  = ioctl( audio_fd, SNDCTL_DSP_SETTRIGGER, &tmp );
-	if ( rc < 0 ) {
-		perror( snddevice->string );
-		Com_Printf( "Could not toggle.\n" );
-		close( audio_fd );
-		return 0;
-	}
-
-	tmp = PCM_ENABLE_OUTPUT;
-	rc = ioctl( audio_fd, SNDCTL_DSP_SETTRIGGER, &tmp );
-	if ( rc < 0 ) {
-		perror( snddevice->string );
-		Com_Printf( "Could not toggle.\n" );
-		close( audio_fd );
-
-		return 0;
-	}
+	__SDL_PauseAudio(0); // start callback
 
 	snd_inited = 1;
-	return 1;
+	return qtrue;
 }
 
 int SNDDMA_GetDMAPos( void ) {
-	struct count_info count;
-
 	if ( !snd_inited ) {
 		return 0;
 	}
 
-	if ( ioctl( audio_fd, SNDCTL_DSP_GETOPTR, &count ) == -1 ) {
-		perror( snddevice->string );
-		Com_Printf( "Uh, sound dead.\n" );
-		close( audio_fd );
-		snd_inited = 0;
-		return 0;
-	}
-	return count.ptr / ( dma.samplebits / 8 );
+	return sdl_state.dma_pos;
 }
 
 void SNDDMA_Shutdown( void ) {
+	__SDL_PauseAudio(1);
+	__SDL_CloseAudio();
+	__SDL_QuitSubSystem(SDL_INIT_AUDIO);
+
+	if ( dma.buffer != NULL ) {
+		free( dma.buffer );
+		dma.buffer = NULL;
+	}
+
+	sdl_state.dma_pos = 0;
+	sdl_state.dma_size = 0;
+	snd_inited = 0;
 }
 
 /*
@@ -303,7 +251,48 @@ Send sound to device if buffer isn't really the dma buffer
 ===============
 */
 void SNDDMA_Submit( void ) {
+	__SDL_UnlockAudio();
 }
 
 void SNDDMA_BeginPainting( void ) {
+	__SDL_LockAudio();
+}
+
+static void sdl_callback(void *userdata, Uint8 *stream, int len) {
+	int tobufend;
+	int len1;
+	int len2;
+	int pos = ( sdl_state.dma_pos * ( dma.samplebits / 8 ) );
+
+	if ( pos > sdl_state.dma_size ) {
+		sdl_state.dma_pos = 0;
+		pos = 0;
+	}
+
+	if ( !snd_inited ) {
+		memset( stream, '\0', len );
+		return;
+	}
+
+	tobufend = sdl_state.dma_size - pos;
+	len1 = len;
+	len2 = 0;
+
+	if ( len1 > tobufend ) {
+		len1 = tobufend;
+		len2 = len - len1;
+	}
+
+	memcpy( stream, dma.buffer + pos, len1 );
+
+	if ( len2 <= 0 ) {
+		sdl_state.dma_pos += ( len1 / ( dma.samplebits / 8 ) );
+	} else {
+		memcpy( stream + len1, dma.buffer, len2 );
+		sdl_state.dma_pos = ( len2 / ( dma.samplebits / 8 ) );
+	}
+
+	if ( sdl_state.dma_pos >= sdl_state.dma_size ) {
+		sdl_state.dma_pos = 0;
+	}
 }
